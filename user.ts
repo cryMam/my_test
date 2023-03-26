debd8bec53bc31bbabd6e865e3a0a8715869161a
cryMam <128988301+cryMam@users.noreply.github.com>
2023-03-26 17:24:06 +0200

    Create user.ts

---
+++ user.ts
@@ -0,0 +1,28 @@
+export enum UserActionTypes {
+  FETCH_USERS = 'FETCH_USERS',
+  FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS',
+  FETCH_USERS_ERROR = 'FETCH_USERS_ERROR',
+}
+
+interface FetchUsersAction {
+  type: UserActionTypes.FETCH_USERS;
+}
+interface FetchUsersSuccessAction {
+  type: UserActionTypes.FETCH_USERS_SUCCESS;
+  payload: any[];
+}
+interface FetchUsersErrorAction {
+  type: UserActionTypes.FETCH_USERS_ERROR;
+  payload: string;
+}
+
+export interface UserState {
+  users: any[];
+  loading: boolean;
+  error: null | string;
+}
+
+export type UserAction =
+  | FetchUsersAction
+  | FetchUsersSuccessAction
+  | FetchUsersErrorAction;
